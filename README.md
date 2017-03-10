9ae10a5b3b48a87d5591b370ed930f5d65b5cc97
Dmitriy Studinskiy <dms@smartling.com>
2017-03-10 11:15:04 +0200

    Updated readme for publishing on marketplace

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# wordpress-divi-builder-localization-plugin
+# Divi Builder localization
+This plugin was developed in addition to [WordPress Smartling Connector](https://wordpress.org/plugins/smartling-connector/) ([source code](https://github.com/Smartling/wordpress-localization-plugin)). It extends functionality of Smartling Connector by adding localization support for [Divi Builder](https://www.elegantthemes.com/plugins/divi-builder/). In a nutshell it just provides additional configuration how to work with Divi Builder shortcodes:
+* Shortcodes should be protected from translator
+* Shortcode attributes may contain content or reference to post\category\etc. Smartling translates content and localizes references
+* Rest attributes are cloned from an original post into translated
--- smartling-divi-builder.php
+++ smartling-divi-builder.php
@@ -1,33 +1,30 @@
 <?php
 
 /**
- * @link              https://www.smartling.com
- * @since             1.0.0
- * @package           smartling-divi-builder
- * @wordpress-plugin
- * Plugin Name:       Smartling Divi Builder Extension
- * Plugin URI:        https://www.smartling.com/translation-software/wordpress-translation-plugin/
- * Description:       Integrate your Wordpress site with Smartling to upload your content and download translations.
- * Version:           1.0.0
- * Author:            Smartling
- * Author URI:        https://www.smartling.com
- * License:           GPL-2.0+
- * Network:           true
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- */
+  * @link              https://www.smartling.com
+  * @since             1.0.0
+  * @package           smartling-divi-builder-localization
+  * @wordpress-plugin
+  * Plugin Name:       Smartling Divi Builder localization
+  * Description:       Extend Smartling Connector functionality to support localization of Divi Builder shortcodes
+  * Plugin URI:        https://www.smartling.com/translation-software/wordpress-translation-plugin/
+  * Author URI:        https://www.smartling.com
+  * License:           GPL-3.0+
+  * Network:           true
+  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+  * Version: 1.0
+*/
 
 add_action('plugins_loaded', function () {
     add_action('smartling_before_init', function (\Symfony\Component\DependencyInjection\ContainerBuilder $di) {
         // Require files.
         require_once __DIR__ . '/extension/ShortcodeInjector.php';
         require_once __DIR__ . '/SmartlingDiviBuilderTagProcessor.php';
-        
+
         // Register shortcodes.
         SmartlingDiviBuilder\SmartlingDiviBuilderTagProcessor::registerShortcodes();
-        
+
         // Register filters.
         SmartlingDiviBuilder\SmartlingDiviBuilderTagProcessor::registerFilters();
     });
 });
-
-
